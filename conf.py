8855c28cf9349501a6968d1ff525cadfd688b799
Ivan Font <ifont@redhat.com>
2016-12-11 00:49:05 -0800

    Nikola auto commit.
    Nikola version: 7.8.1

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Ivan Font"  # (translatable)
 BLOG_TITLE = "Ivan Font"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://font.github.io/"
+SITE_URL = "http://ivanfont.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://font.github.io/"
